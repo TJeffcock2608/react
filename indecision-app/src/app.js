104e1d12e1fd02d3b7415e0edaddb5aeed771d27
thomasjeffcock <thomas.jeffcock@nihp.nhs.uk>
2021-03-05 16:10:50 +0000

    Installing Babel

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,15 +1,9 @@
-'use strict';
-
 console.log('App.js is running!');
 
 // JSX - JavaScript XML 
 
 // var template = <p>This is JSX from App.js</p>;
-var template = React.createElement(
-  'h1',
-  null,
-  'Indeceision app'
-);
+var template = <h1>Indeceision app</h1>
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
